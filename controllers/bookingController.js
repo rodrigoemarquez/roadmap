6cceaed8ae71b55ba12cbd153d96bc9f5f41c68e
rodrigoemarquez <rodrigoemarquez@hotmail.com>
2023-01-03 10:07:24 +1100

    Update bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -73,7 +73,7 @@ const createBookingCheckout = async session => {
   await Booking.create({ tour, user, price });
 };
 
-exports.webhookCheckout = (req, res, next) => {
+exports.webhookCheckout = async (req, res, next) => {
   const signature = req.headers['STRIPE_SIGNATURE'];
   let event;
   try {
@@ -85,9 +85,8 @@ exports.webhookCheckout = (req, res, next) => {
     if (event.type === 'checkout.session.completed') {
       createBookingCheckout(event.data.object);
     }
-  
-    res.status(200).json({ received: true });
 
+    res.status(200).json({ received: true });
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
