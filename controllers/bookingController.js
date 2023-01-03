740cbd0854d812c0b5177a04cb32b92c2f4de697
rodrigoemarquez <rodrigoemarquez@hotmail.com>
2023-01-03 11:35:20 +1100

    Update bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -65,29 +65,31 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 //   res.redirect(req.originalUrl.split('?')[0]);
 // };
 
-const createBookingCheckout = async (event) => {
+const createBookingCheckout = async event => {
   const tour = event.data.client_reference_id;
   const user = (await User.findOne({ email: event.data.object.email })).id;
   const price = event.data.object.amount_total;
   await Booking.create({ tour, user, price });
 };
 
-exports.webhookCheckout = async (req, res) => {
+exports.webhookCheckout = (req, res) => {
   const signature = req.headers['stripe-signature'];
   let event;
   try {
-    event = await stripe.webhooks.constructEvent(
+    event = stripe.webhooks.constructEvent(
       req.body,
       signature,
       process.env.STRIPE_WEBHOOK_SECRET
     );
-    if (event.type === 'checkout.session.completed') {
-      createBookingCheckout();
-      res.status(200).json({ received: true });
-    }
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
+
+  if (event.type === 'checkout.session.completed') {
+    createBookingCheckout();
+  }
+
+  res.status(200).json({ received: true });
 };
 
 exports.getAllBookings = factory.getAll(Booking);
