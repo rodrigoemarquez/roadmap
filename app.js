0b52230be6b9a34cf5ba5d96a53bb785d03a926f
rodrigoemarquez <rodrigoemarquez@hotmail.com>
2023-01-03 11:45:56 +1100

    Testing Stripe Webhooks

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const bookingController = require('./controllers/bookingController');
 
 const AppError = require('./utils/appError');
@@ -53,7 +54,7 @@ app.use('/api', limiter);
 
 app.post(
   '/webhook-checkout',
-  express.raw({ type: '*/*' }),
+  bodyParser.raw(),
   bookingController.webhookCheckout
 );
 
