6ec68eb3d427255d414aa10808f1048fef695fe1
rodrigoemarquez <rodrigoemarquez@hotmail.com>
2023-01-03 12:25:10 +1100

    Update bookingController.js

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -72,7 +72,7 @@ exports.webhookCheckout = (req, res) => {
     event = stripe.webhooks.constructEvent(
       req.body,
       signature,
-      Buffer.from(process.env.STRIPE_WEBHOOK_SECRET)
+      process.env.STRIPE_WEBHOOK_SECRET
     );
   } catch (err) {
     return res.status(400).send(`Webhook error: ${err.message}`);
